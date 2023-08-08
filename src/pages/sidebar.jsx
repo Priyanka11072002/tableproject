0a8f5a6ec70fa81e2e28dd0e221ec8d83955a3ed
Priyanka11072002 <pk796395+pk@gmail.com>
2023-08-08 17:59:12 +0530

    updated

--- src/pages/sidebar.jsx
+++ src/pages/sidebar.jsx
@@ -1,28 +1,14 @@
 import React,{useContext} from 'react'
 import "./header.css";
-import {useState } from 'react';
 import Store from './context';
 
 const Sidebar = () => {
 
   const {toggle1,handleToggle1} = useContext(Store)
-    //const [toggle,setToggle]  = useState(false)
-//  const [sidebar,setSidebar]  = useState()
-//  useEffect(()=>{
-//     setSidebar(toggle)  
   
-  
-//    },[sidebar,toggle])
-//  const handleToggle = ()=>{
-//     setToggle(!toggle)
-//    }
   return (
     <>
     
-{/* <i className={`fa-solid fa-bars  ${toggle?'toggle':''}`} onClick={()=>setToggle(pre=>!toggle)} ></i>
-<i className="fa-solid fa-bars" onClick={()=>setToggle(pre=>!toggle)} ></i> */}
-{/* {sidebar?<div className={`sidebar ${sidebar?'toggle-sidebar':''}`}> */}
-
 
 <div className={`sidebar ${toggle1?'collapsed':''}`}>
 <i className='fa-solid fa-bars' onClick={handleToggle1}></i>
